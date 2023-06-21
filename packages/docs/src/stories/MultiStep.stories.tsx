04882fe9618bb5de723d8985f8d2722b703ecb4d
Luciano Baraúna <baraunaluciano@gmail.com>
2023-06-21 16:06:17 -0300

    primeiro commit e testando aula CI

---
+++ packages/docs/src/stories/MultiStep.stories.tsx
@@ -0,0 +1,32 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Box, MultiStep, MultiStepProps } from '@ignite-ui-lbl/react'
+
+export default {
+  title: 'Form/MultiStep',
+  component: MultiStep,
+  tags: ['autodocs'],
+  args: {
+    currentStep: 1,
+    size: 4,
+  },
+  decorators: [
+    (Story) => {
+      return (
+        <Box
+          as="label"
+          css={{ display: 'flex', flexDirection: 'column', gap: '$2' }}
+        >
+          {Story()}
+        </Box>
+      )
+    },
+  ],
+} as Meta<MultiStepProps>
+
+export const Primary: StoryObj<MultiStepProps> = {}
+
+export const Full: StoryObj<MultiStepProps> = {
+  args: {
+    currentStep: 4,
+  },
+}
